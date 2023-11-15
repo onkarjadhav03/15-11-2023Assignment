7d1c04338914f73cfa5768038366ddf9b98533d5
onkar <jadhavonkar333@gmail.com>
2023-11-15 15:40:50 +0530

    Assignment13

---
+++ Assignment13/Mtds.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    public class Mtds
+    {
+        public void userinp(string str)
+        {
+            string[] words = str.Split(' ');
+
+            Console.WriteLine("number of words"+words.Length);
+        }
+
+        public void mail(string str)
+        {
+            string pattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            Regex regex = new Regex(pattern);
+            if(regex.IsMatch(str))
+            {
+                Console.WriteLine("valid email");
+            }
+            else
+            {
+                Console.WriteLine("invalid email");
+            }
+        }
+
+        public void mobile(string number)
+        {
+            string pattern = @"^\d{10}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(number))
+            {
+                Console.WriteLine("valid mobile no");
+            }
+            else
+            {
+                Console.WriteLine("invalid mobile no");
+            }
+        }
+
+        public void PerformCustomRegexSearch(string text, string pattern)
+        {
+            try
+            {
+                // Create a Regex object with the custom pattern
+                Regex regex = new Regex(pattern);
+
+                // Use the Matches method to find all matches in the text
+                MatchCollection matches = regex.Matches(text);
+
+                // Display the matches found
+                if (matches.Count > 0)
+                {
+                    Console.WriteLine($"Matches found ({matches.Count}):");
+                    foreach (Match match in matches)
+                    {
+                        Console.WriteLine($"  {match.Value} at index {match.Index}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No matches found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Assignment13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter choice:1.word count\n2.email-validation\n3.mobile validation\n4.custom validation");
+            int ch=int.Parse(Console.ReadLine());
+            
+            Mtds mtds = new Mtds();
+            if (ch == 1)
+            {
+                Console.WriteLine("enter text");
+                string st = Console.ReadLine();
+                mtds.userinp(st);
+            }
+            else if(ch == 2)
+            {
+                Console.WriteLine("enter email");
+                string st = Console.ReadLine();
+                mtds.mail(st);
+            }
+            else if(ch==3)
+            {
+                Console.WriteLine("enter text");
+                string st = Console.ReadLine();
+                mtds.mobile(st);
+            }
+
+            if (ch == 4)
+            {
+                Console.Write("Enter a text for regex search: ");
+                string inputText = Console.ReadLine();
+                Console.Write("Enter a custom regular expression: ");
+                string customRegex = Console.ReadLine();
+                mtds.PerformCustomRegexSearch(inputText, customRegex);
+
+            }
+        }
+    }
+}
